18b91509e9e73dd54f5a19f256877bcc0c992e2f
Paul <Sivivatu@users.noreply.github.com>
2020-01-28 10:11:08 +0000

    changed folder name to clean up folders

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ module "networking" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 module "alteryx-testing" {
-  source = "../compute"
+  source = "../worker"
   server_port = 80
 }
 
