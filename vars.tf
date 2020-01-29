7da59a220c5c1833180093c6b1034a0044ec2c1f
Paul <Sivivatu@users.noreply.github.com>
2020-01-29 17:56:27 +0000

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -61,8 +61,8 @@ variable "admin_password" {
 }
 
 # Required for local development.
-variable "AWS_ACCESS_KEY_ID" {
-}
+#variable "AWS_ACCESS_KEY_ID" {
+#}
 
-variable "AWS_SECRET_ACCESS_KEY" {
-}
+#variable "AWS_SECRET_ACCESS_KEY" {
+#}
