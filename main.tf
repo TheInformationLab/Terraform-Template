e9710e7fee33ab6c4e7235db24f71a513a32252f
Paul <Sivivatu@users.noreply.github.com>
2020-02-07 12:54:29 +0000

    add password output

--- main.tf
+++ main.tf
@@ -74,10 +74,10 @@ resource "aws_instance" "server" {
 }
 
 # TODO: Consider if the default password is required for remote access in the scripted process. Should this be manually accessed 
-#output "ec2_password" {
-#    provisioner "local-exec" {
-#     command = "echo ${aws_instance.web.private_ip} >> private_ips.txt"
-#   }
-#   ## Need to provide your own .pem key that can be created in AWS or on your machine for each provisioned EC2.
-#   value = rsadecrypt(aws_instance.server[0].password_data,  "${data.aws_s3_bucket_object.secret_key.body}")
-# }
+output "ec2_password" {
+   provisioner "local-exec" {
+    command = "echo ${aws_instance.web.private_ip} >> private_ips.txt"
+  }
+  ## Need to provide your own .pem key that can be created in AWS or on your machine for each provisioned EC2.
+  value = rsadecrypt(self.password_data, "${data.aws_s3_bucket_object.secret_key.body}")
+}
